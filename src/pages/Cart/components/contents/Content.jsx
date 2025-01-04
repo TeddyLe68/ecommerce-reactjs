2fccf296938aace0c36003e9726762769df732a8
TeddyLe68 <teddyle2003@gmail.com>
2025-01-04 08:57:58 +0700

    UI/UX view cart

--- src/pages/Cart/components/contents/Content.jsx
+++ src/pages/Cart/components/contents/Content.jsx
@@ -1,12 +1,25 @@
+import Button from "@components/Button/Button";
 import styles from "../../styles.module.scss";
 import CartSummary from "./CartSummary";
 import CartTable from "./CartTable";
 function Content() {
-  const { containerContent } = styles;
+  const { containerContent, boxFooter, boxBtnDelete, boxCoupon } = styles;
   return (
     <div className={containerContent}>
       <div>
         <CartTable />
+        <div className={boxFooter}>
+          <div className={boxCoupon}>
+            <input type="text" placeholder="Coupon code" />
+            <Button content={"OK"} isPrimary={false} />
+          </div>
+          <div className={boxBtnDelete}>
+            <Button
+              content={<div>🗑️ Clear shopping cart</div>}
+              isPrimary={false}
+            />
+          </div>
+        </div>
       </div>
       <CartSummary />
     </div>
