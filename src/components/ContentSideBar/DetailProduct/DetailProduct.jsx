cac6e93aa458c2860478dc1b56165a4317a08e44
TeddyLe68 <teddyle2003@gmail.com>
2025-01-14 08:58:56 +0700

    [Content sidebar]: detail products

--- src/components/ContentSideBar/DetailProduct/DetailProduct.jsx
+++ src/components/ContentSideBar/DetailProduct/DetailProduct.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PiShoppingCartLight } from "react-icons/pi";
 import { TfiReload } from "react-icons/tfi";
 import { PiHeartLight } from "react-icons/pi";
@@ -9,6 +9,8 @@ import SliderCommon from "@/components/SliderCommon/SliderCommon";
 import styles from "./styles.module.scss";
 import SelectBox from "@/pages/OurShop/components/SelectBox";
 import Button from "@components/Button/Button";
+import cls from "classnames";
+import { addProductToCart } from "@/apis/cartService";
 function DetailProduct() {
   const {
     container,
@@ -24,8 +26,18 @@ function DetailProduct() {
     or,
     boxAddOther,
     boxFooter,
+    isActive,
   } = styles;
-  const { detailProduct } = useContext(SideBarContext);
+  const {
+    detailProduct,
+    userId,
+    setType,
+    handleGetListProductCart,
+    setIsLoading,
+    setIsOpen,
+  } = useContext(SideBarContext);
+  const [chooseSize, setChooseSize] = useState("");
+  const [quantity, setQuantity] = useState("1");
   const showOption = [
     { label: "1", value: "1" },
     { label: "2", value: "2" },
@@ -35,7 +47,38 @@ function DetailProduct() {
     { label: "6", value: "6" },
     { label: "7", value: "7" },
   ];
-  console.log(detailProduct);
+  const handleGetSize = (value) => {
+    setChooseSize(value);
+  };
+
+  const handleDeleteSize = () => {
+    setChooseSize("");
+  };
+  const handleGetQuantity = (value) => {
+    setQuantity(value);
+  };
+
+  const handleAddToCart = () => {
+    const data = {
+      userId: userId,
+      productId: detailProduct._id,
+      quantity,
+      size: chooseSize,
+      isMultiple: true,
+    };
+    setIsOpen(false);
+    setIsLoading(true);
+    addProductToCart(data)
+      .then((res) => {
+        setIsOpen(true);
+        setType("cart");
+        handleGetListProductCart("cart", userId);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className={container}>
       <SliderCommon data={detailProduct.images} />
@@ -44,17 +87,39 @@ function DetailProduct() {
       <div className={price}>${detailProduct.price}</div>
       <div className={des}>{detailProduct.description}</div>
 
-      <div className={label}>Size</div>
+      <div className={label}>Size {chooseSize}</div>
       <div className={boxSize}>
         {detailProduct.size.map((item, index) => (
-          <div key={index} className={size}>
+          <div
+            key={index}
+            className={cls(size, { [isActive]: chooseSize === item.name })}
+            onClick={() => handleGetSize(item.name)}
+          >
             {item.name}
           </div>
         ))}
       </div>
 
+      {chooseSize && (
+        <div
+          style={{
+            fontSize: "12px",
+            marginTop: "3px",
+            cursor: "pointer",
+          }}
+          onClick={handleDeleteSize}
+        >
+          clear
+        </div>
+      )}
+
       <div className={boxAddToCart}>
-        <SelectBox options={showOption} />
+        <SelectBox
+          options={showOption}
+          type="show"
+          defaultValue={quantity}
+          getValue={handleGetQuantity}
+        />
         <div>
           <Button
             content={
@@ -62,6 +127,7 @@ function DetailProduct() {
                 <PiShoppingCartLight /> ADD TO CART
               </div>
             }
+            onClick={handleAddToCart}
           />
         </div>
       </div>
